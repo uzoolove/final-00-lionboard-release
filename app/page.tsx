cb62e87be6c0329323c93bb9f090ecf7c1a2f6a8
uzoolove <uzoolove@gmail.com>
2026-01-14 07:42:58 +0000

    .env 변수값 출력

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>final-project-sample v02</h1>
+    <>
+      <h1>final-project-sample v02</h1>
+      <h2>client-id: { process.env.NEXT_PUBLIC_CLIENT_ID }</h2>
+    </>
   );
 }
